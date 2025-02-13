868389b5257282d95a001ff4b98a4bfb7cee33a7
Jacob Mendez <Jacobmendez1105@gmail.com>
2025-02-13 08:45:28 -0500

    Bad Mapping Complete

--- HCI/BadMapping.js
+++ HCI/BadMapping.js
@@ -1,38 +1,67 @@
+//Variables to store letter positions and the letters generated
+var lx = 0;
+var ly = 0;
+var letters = [];
 
-// The goal is to build a horrible interface
-
-//CONCEPT: Using an oldschool locker turndial for numerical input.
-
-function setUp(){
-    createCanvas(400, 400);
-    angleMode(DEGREES); //Set the angle mode to degrees
+function setup() {
+  createCanvas(400, 400);
+  lx = Math.random() * 400;
+  ly = Math.random() * 400;
+  renderLetters(lx, ly);
 }
 
-function drawDial(x, y, r) {
-// First step is to predefine a cricle
-    stroke(0);
-    strokeWeight(4);
-    fill(255);
-    ellipse(x, y, r, r); // Draw the outer circle
+//function to render letters to screen, generates n letters across the canvas
+function renderLetters(x, y){
+  push();
+  stroke("black");
+  fill("white");
+  textSize(32);
+  letters = [];
+  //create a list of letters to generate. Change i to generate more or less.
+  for(var i = 0; i < 34; i++){
+    let char = String.fromCharCode(65 + Math.random()*i); //generate chars that are alphabetical letters from A to Z.
+    let posX = x + Math.random()*400;
+    let posY = y + Math.random()*400;
+    text(char, posX, posY);
+    letters.push({char: char, x: posX, y: posY});
+  }
+  pop();
+}
 
-    //Add tick lines around the circle
-    let numTicks = 10; // Number of ticks
-    let tickLength = 20; // Length of the tick marks
-    let tickWidth = 4; // Width of the tick marks
-    let tickAngle = 360 / numTicks; // Angle between each tick
-    for (let i = 0; i < numTicks; i++){
-        let angle = i * tickAngle; //Each angle should have a tick
-        // Each tick is 20 pixels long and will need an x and y coordinate for the start and ending points
-        let x1 = x + (r/2)*cos(angle); 
-        let y1 = y + (r/2)*sin(angle);
-        let x2 = x + (r/2 + tickLength)*cos(angle); //Here we want to add the tick length so that the tick is long.
-        let y2 = y + (r/2 + tickLength)*sin(angle);
-        strokeWeight(tickWidth);
-        line(x1, y1, x2, y2);
+function renderResetBox() {
+  //A reset box lets the user reset the values that can be selected. However, the program does not wipe the old letters off the canvas, but does make them unusable.
+  push();
+  stroke("gray");
+  fill("gray");
+  rect(0,0,50,50);
+  stroke("black");
+  fill("black");
+  text("Reset", 1, 25);
+}
+
+function mouseClicked(){
+  //Checks to see if the mouse was clicked in a meaningful location.
+
+  //Check if the mouse is in the reset box.
+  if(mouseX < 50 && mouseY < 50){
+    letters = [];
+    lx = Math.random() * 400;
+    ly = Math.random() * 200;
+    renderLetters(lx, ly);
+  }
+  //Check if the mouse clicked a letter
+  for (let i = 0; i < letters.length; i++) {
+        let letter = letters[i];
+        if (dist(mouseX, mouseY, letter.x, letter.y) < 25) {
+            console.log(letter.char);
+            break;
+        }
     }
 }
 
-function draw(){
-    background(220);
-    drawDial(200, 200, 200); // Draw the dial
+function draw() {
+  background(220);
+  renderLetters(lx, ly);
+  renderResetBox();
+  noLoop();
 }

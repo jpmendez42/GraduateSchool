b33e929a8b370709b0144952470d0e0d2a66fd39
Jacob Mendez <Jacobmendez1105@gmail.com>
2025-07-22 05:23:39 -0400

    Machinehpp written

--- Graduate School/Computer Systems/Machine.hpp
+++ Graduate School/Computer Systems/Machine.hpp
@@ -20,7 +20,11 @@ class Machine {
     uint32_t read_register(uint8_t reg) const;
     void write_to_register(uint32_t address, uint8_t value);
     
-    //
+    //Memory access
+    uint8_t read_memory(uint32_t address) const;
+    void write_to_memory(uint32_t address, uint8_t val);
+
+    static uint32_t sign_extend(uint32_t val, uint8_t sign_index);
              
 
 };

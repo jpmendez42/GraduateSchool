a2d5497dda2ba39e11c722af9970c8a55696f824
Jacob Mendez <Jacobmendez1105@gmail.com>
2025-07-22 20:12:44 -0400

    Project Machine Reset

--- Graduate School/Computer Systems/Machine.hpp
+++ Graduate School/Computer Systems/Machine.hpp
@@ -18,11 +18,15 @@ class Machine {
 
     //Register access:
     uint32_t read_register(uint8_t reg) const;
-    void write_to_register(uint32_t address, uint8_t value);
+    void write_register(uint8_t reg, uint32_t value);
     
     //Memory access
     uint8_t read_memory(uint32_t address) const;
     void write_to_memory(uint32_t address, uint8_t val);
+    
+    //PC access
+    uint32_t get_pc() const;
+    void set_pc(uint32_t val);
 
     static uint32_t sign_extend(uint32_t val, uint8_t sign_index);
              

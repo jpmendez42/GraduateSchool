32e3da9413c6ac9715ac1ed43ea8039fb249dd1c
Jacob Mendez <Jacobmendez1105@gmail.com>
2025-07-22 20:34:39 -0400

    Memory.cpp completed

---
+++ Graduate School/Computer Systems/Memory.cpp
@@ -0,0 +1,11 @@
+#include "Memory.hpp"
+
+Memory::Memory() : mem{} {}
+
+uint8_t Memory::read_byte(uint32_t address) const {
+    return mem[address % size];
+}
+
+void Memory::write_byte(uint32_t address, uint8_t value) {
+    mem[address % size] = value;
+}

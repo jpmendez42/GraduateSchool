319a6f9f6552e9804d5987cdca0fbc10a7008e71
Jacob Mendez <Jacobmendez1105@gmail.com>
2025-07-22 20:26:24 -0400

    Machine.cpp reworked

--- Graduate School/Computer Systems/Machine.cpp
+++ Graduate School/Computer Systems/Machine.cpp
@@ -1,61 +1,28 @@
 
 #include "Machine.hpp"
 
-Machine::Machine() {
-    registers[0]  = 0;
-    registers[1]  = 0;
-    registers[2]  = RAM_SIZE; 
-    registers[3]  = 0;
-    registers[4]  = 0;
-    registers[5]  = 0;
-    registers[6]  = 0;
-    registers[7]  = 0;
-    registers[8]  = 0;
-    registers[9]  = 0;
-    registers[10] = 0;
-    registers[11] = 0;
-    registers[12] = 0;
-    registers[13] = 0;
-    registers[14] = 0;
-    registers[15] = 0;
-    registers[16] = 0;
-    registers[17] = 0;
-    registers[18] = 0;
-    registers[19] = 0;
-    registers[20] = 0;
-    registers[21] = 0;
-    registers[22] = 0;
-    registers[23] = 0;
-    registers[24] = 0;
-    registers[25] = 0;
-    registers[26] = 0;
-    registers[27] = 0;
-    registers[28] = 0;
-    registers[29] = 0;
-    registers[30] = 0;
-    registers[31] = 0;
+Machine::Machine() {}
+
+void Machine::write_register(uint8_t reg, uint32_t value) {
+    registers.write(reg, value);
 }
 
-uint32_t Machine::read_register(uint8_t reg) const {
-    return registers[reg & 31] * (reg != 0);
+uint32_t Machine:: read_register(uint8_t reg) const {
+    return registers.read(reg);
 }
 
-void Machine::write_register(uint8_t reg, uint32_t val) {
-    registers[reg & 31] = val * (reg != 0);
+void Machine::write_memory(uint32_t address, uint8_t value) {
+    memory.write_byte(address, value);
 }
 
 uint8_t Machine::read_memory(uint32_t address) const {
-    return memory[address];
-}
-
-void Machine::write_to_memory(uint32_t address, uint8_t val) {
-    memory[address] = val;
+    return memory.read_byte(address);
 }
 
 uint32_t Machine::get_pc() const {
-    return progCounter;
+    return registers.get_pc()
 }
 
-void Machine::set_pc(uint32_t val) {
-    progCounter = val;
+void Machine::set_pc(uint32_t value) {
+    registers.set_pc(value);
 }
